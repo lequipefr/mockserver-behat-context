6d66517333c0daae1f02585d3911f9479ce9a296
Julien Maulny <jmaulny-ext@lequipe.fr>
2022-10-17 14:56:49 +0200

    [Fixes #2] Prevent expectations being still active on next scenarios

--- tests/MockServerContextTest.php
+++ tests/MockServerContextTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Lequipe\Test\MockServer;
 
 use Behat\Gherkin\Node\PyStringNode;
+use Lequipe\MockServer\Exception\Exception;
 use Lequipe\MockServer\MockServerClient;
 use Lequipe\MockServer\MockServerContext;
 use PHPUnit\Framework\TestCase;
@@ -36,7 +37,7 @@ class MockServerContextTest extends TestCase
         $context->theRequestOnApiWillReturnJson('get', '/users/1', new PyStringNode(['{}'], 0));
     }
 
-    public function testResetIsNotCalledIfNotApiCall()
+    public function testResetIsNotCalledIfNoApiCall()
     {
         $client = $this->prophesize(MockServerClient::class);
 
@@ -48,5 +49,73 @@ class MockServerContextTest extends TestCase
         $context = new MockServerContext($client->reveal());
 
         $context->beforeScenario();
+        $context->afterScenario();
+    }
+
+    public function testResetIsNotCalledOnTheEndOfScenatioIfNotNecessary()
+    {
+        $client = $this->prophesize(MockServerClient::class);
+
+        $client
+            ->reset()
+            ->shouldBeCalledTimes(0)
+        ;
+
+        $client
+            ->expectation(Argument::any())
+        ;
+
+        $context = new MockServerContext($client->reveal());
+
+        $context->beforeScenario();
+        $context->afterScenario();
+    }
+
+    public function testResetIsCalledOnTheEndOfScenatioIfNecessary()
+    {
+        $client = $this->prophesize(MockServerClient::class);
+
+        $client
+            ->reset()
+            ->shouldBeCalledTimes(2)
+        ;
+
+        $client
+            ->expectation(Argument::any())
+        ;
+
+        $context = new MockServerContext($client->reveal());
+
+        $context->beforeScenario();
+        $context->theRequestOnApiWillReturnJson('get', '/users', new PyStringNode(['[]'], 0));
+        $context->theRequestOnApiWillReturnJson('get', '/users/1', new PyStringNode(['{}'], 0));
+        $context->afterScenario();
+    }
+
+    /**
+     * Functions like "iWillReceiveTheHeader" add request elements to assert
+     * from the next expectation, but the expectation is not send until
+     * phrases like "theRequestOnApiWillReturnJson".
+     *
+     * Context throw an exception if at the end of a scenario,
+     * an expectation is still being built, but has not been sent.
+     */
+    public function testAfterScenarioCheckNotSentExpectations()
+    {
+        $client = $this->prophesize(MockServerClient::class);
+
+        $client
+            ->expectation(Argument::any())
+        ;
+
+        $context = new MockServerContext($client->reveal());
+
+        $context->beforeScenario();
+        $context->iWillReceiveTheHeader('Header', 'value');
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('An expectation is currently building and has not been sent');
+
+        $context->afterScenario();
     }
 }
